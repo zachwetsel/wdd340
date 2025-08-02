bff2c2f47dbbdada267aeb46889f23b645f31213
zachwetsel <zachwetsel@yahoo.com>
2025-08-02 12:36:45 -0600

    Added Grid

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require('express-ejs-layouts')
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +25,9 @@ app.set("layout", "./layouts/layout") // bot at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
